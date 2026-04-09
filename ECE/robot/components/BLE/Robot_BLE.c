76ed9bc2c60586b87fe4917ab1445a6c7058e098
Mason <mmmoore456@gmail.com>
2026-04-09 16:14:16 -0400

    replace ESP_LE_AUTH_REQ_MITM_BOND command with compatible code 5

--- ECE/robot/components/BLE/Robot_BLE.c
+++ ECE/robot/components/BLE/Robot_BLE.c
@@ -203,7 +203,17 @@ void robot_ble_init() {
     /*BEFORE*/
     //esp_ble_auth_req_t auth_req = ESP_LE_AUTH_REQ_SC_MITM_BOND;
     
-    esp_ble_auth_req_t auth_req = ESP_LE_AUTH_REQ_MITM_BOND;  /* remove SC */
+    //esp_ble_auth_req_t auth_req = ESP_LE_AUTH_REQ_MITM_BOND;  /* remove SC */
+
+    /*
+    0 = No security
+    1 = Bond only
+    4 = MITM only  
+    5 = MITM + Bond          ← what we want
+    13 = SC + MITM + Bond    ← what we had before
+    */
+
+    esp_ble_auth_req_t auth_req = (esp_ble_auth_req_t)5;  /* MITM + BOND, no SC */
     esp_ble_io_cap_t iocap = ESP_IO_CAP_OUT;  /* ESP32 displays passkey, ground station enters it */
 
 
